e6e3a8cb4c7caa0853cb2e7e06f4df15b93da666
CoCoSol007 <solois.corentin@gmail.com>
2023-10-24 18:40:31 +0200

    update

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 80;
+const path = require('path');
+
+
+
+
+const mainRoute = require('./routes/mainRoute'); 
+const dataRoute = require('./routes/dataRoute'); 
+const articleRoute = require('./routes/articleRoute'); 
+const adminRoute = require('./routes/adminRoute'); 
+
+
+// Utilisez les routes en tant que middleware
+app.use('/', mainRoute);
+app.use('/admin', adminRoute)
+app.use('/data', dataRoute);
+app.use('/article', articleRoute);
+
+app.use((req, res, next) => {
+  res.redirect("/?file=page_not_found.html")
+});
+
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
---
+++ routes/articleRoute.js
@@ -0,0 +1,79 @@
+// routes.js
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+
+
+router.get("/:id", (req, res) => {
+    const articleId = req.params.id;
+  
+    fs.readFile("routes/data/data.json", "utf8", (err, data) => {
+      if (err) {
+        console.error("Erreur de lecture du fichier de données :", err);
+        res.status(500).send("Erreur interne du serveur");
+        return;
+      }
+      const jsonData = JSON.parse(data);
+      const article = jsonData.find(entry => entry.id == articleId);
+      if (article == undefined) {
+        res.redirect("/?file=page_not_found.html");
+        return;
+      }
+      if (article.text === undefined ){
+        article.text = [""]
+      }
+      const formattedText = article.text.map(element => `\`${element}\``);
+  
+      const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="/?file=style.css">
+        </head>
+        <body>
+            <nav>
+                <ul class="navbar">
+                    <li class="nav-item"><a href="/?file=main.html">Home</a></li>
+                    <li class="nav-item"><a href="/?file=articles.html">Articles</a></li>
+                    <li class="nav-item"><a href="/?file=about_me.html">About me</a></li>
+                    <li class="nav-item"><a href="/?file=contact_me.html">Contact me</a></li>
+                </ul>
+            </nav>
+            <main class="main">
+                <h1>${article.title}</h1>
+                <h2>By CoCo Sol</h2> <br>
+                <pre><b><u>${article.intro}</u></b><br></pre>
+                <div class="text-content"></div>
+                <script>
+                    // Sélection de l'élément où insérer le texte formaté
+                    const textContainer = document.querySelector(".text-content");
+                    const textData = [${formattedText}];
+  
+                    for (let i = 0; i < textData.length; i++) {
+                        const title = textData[i];
+                        const txt = textData[i + 1];
+                        const paragraph = document.createElement("p");
+                        paragraph.classList.add("article");
+                        const titleElement = document.createElement("h3");
+                        titleElement.textContent = title;
+                        const textElement = document.createElement("pre");
+                        textElement.textContent = txt;
+                        paragraph.appendChild(titleElement);
+                        paragraph.appendChild(textElement);
+                        textContainer.appendChild(paragraph);
+                        i++; // Pour passer au prochain élément
+                    }
+                </script>
+            </main>
+        </body>
+        </html>
+      `;
+  
+      res.send(htmlContent);
+    });
+  });
+
+// Exportez le router pour l'utiliser dans d'autres fichiers
+module.exports = router;
